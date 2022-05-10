cb22005f8fa35ef4cb028f63f60ed6f26c9282f0
cunyang <cunyangliu@trip.com>
2022-05-10 21:27:32 +0800

    rename

---
+++ src/data/STYLE_LIST.js
@@ -0,0 +1,46 @@
+export default {
+  width: {
+    type: "Input",
+    value: "100%",
+  },
+  height: {
+    type: "Input",
+    value: "100%",
+  },
+  marginTop: {
+    type: "Input",
+    value: "0px",
+  },
+  marginBottom: {
+    type: "Input",
+    value: "0px",
+  },
+  marginLeft: {
+    type: "Input",
+    value: "0px",
+  },
+  marginRight: {
+    type: "Input",
+    value: "0px",
+  },
+  paddingTop: {
+    type: "Input",
+    value: "0px",
+  },
+  paddingBottom: {
+    type: "Input",
+    value: "0px",
+  },
+  paddingLeft: {
+    type: "Input",
+    value: "0px",
+  },
+  paddingRight: {
+    type: "Input",
+    value: "0px",
+  },
+  overflow: {
+    type: "Select",
+    value: "visible",
+  },
+};
