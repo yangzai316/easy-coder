bac6517afad4804f2c8039b25efd2d8d7763fc68
cunyang <cunyangliu@trip.com>
2022-05-19 15:31:02 +0800

    添加路由默认点亮

--- src/components/LeftSider.js
+++ src/components/LeftSider.js
@@ -1,10 +1,18 @@
 import { Menu } from "antd";
 import React from "react";
+import { useLocation } from "react-router-dom";
 import router from "../router";
 
 const LeftSider = () => {
+  const location = useLocation();
   return (
-    <Menu mode="inline" style={{ width: 66 }} theme="dark" items={router} />
+    <Menu
+      mode="inline"
+      style={{ width: 66 }}
+      theme="dark"
+      defaultSelectedKeys={[location.pathname]}
+      items={router}
+    />
   );
 };
 
